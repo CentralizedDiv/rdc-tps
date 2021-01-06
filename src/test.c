c288096b1b5013adfca7f323e6017e03150f2db1
Arthur Melo <arthurhsmelo@gmail.com>
2021-01-06 13:04:39 -0300

    finish subscribe/unsubscribe functions

--- src/test.c
+++ src/test.c
@@ -8,52 +8,54 @@ struct Tag {
   int subscriptions_count;
 } Tag;
 
-int subscribe(int client_socket, char *tag_name, struct Tag **tags, int tags_count) {
+int subscribe(int client_socket, char *tag_name, char ***tags, int *tags_count, int ***subs, int **subs_count) {
   int i;
   int tag_index = -1;
-  for (i = 0; i < tags_count; i++) {
-    if (tags[i]->name != NULL) {
-      if (strcmp(tag_name, tags[i]->name) == 0) {
-        tag_index = i;
-      }
+  for (i = 0; i < *tags_count; i++) {
+    if (strcmp(tag_name, (*tags)[i]) == 0) {
+      tag_index = i;
     }
   }
 
   if (tag_index == -1) {
-    tag_index = tags_count;
-    tags_count = tags_count + 1;
+    tag_index = *tags_count;
+    *tags_count = (*tags_count) + 1;
 
-    *tags = realloc(*tags, tags_count + 1 * sizeof(Tag));
-    tags[tag_index] = malloc(sizeof(Tag));
-    tags[tag_index]->name = tag_name;
-    tags[tag_index]->subscriptions = malloc(sizeof(int));
-    tags[tag_index]->subscriptions_count = 1;
-    tags[tag_index]->subscriptions[0] = client_socket;
+    *tags = realloc(*tags, *tags_count * sizeof(int *));
+    *subs = realloc(*subs, *tags_count * sizeof(int *));
+    *subs_count = realloc(*subs_count, *tags_count * sizeof(int));
 
+    (*tags)[tag_index] = tag_name;
+    (*subs_count)[tag_index] = 1;
+    (*subs)[tag_index] = malloc(sizeof(int));
+    (*subs)[tag_index][0] = client_socket;
+
+    int a = (*subs_count)[0];
+    int b = (*subs_count)[1];
     return 1;
   } else {
     int found_client = 0;
-    for (i = 0; i <= tags[tag_index]->subscriptions_count; i++) {
-      if (tags[tag_index]->subscriptions[i] == client_socket) {
+    for (i = 0; i < (*subs_count)[tag_index]; i++) {
+      if ((*subs)[tag_index][i] == client_socket) {
         found_client = 1;
       }
     }
     if (found_client) {
       return 0;
     } else {
-      tags[tag_index]->subscriptions = realloc(tags[tag_index]->subscriptions, tags[tag_index]->subscriptions_count + 1 * sizeof(int));
-      tags[tag_index]->subscriptions[tags[tag_index]->subscriptions_count] = client_socket;
-      tags[tag_index]->subscriptions_count = tags[tag_index]->subscriptions_count + 1;
+      (*subs_count)[tag_index] = ((*subs_count)[tag_index]) + 1;
+      (*subs)[tag_index] = realloc((*subs)[tag_index], (*subs_count)[tag_index] * sizeof(int));
+      ((*subs)[tag_index])[((*subs_count)[tag_index]) - 1] = client_socket;
       return 1;
     }
   }
 }
 
-int unsubscribe(int client_socket, char *tag_name, struct Tag *tags, int *tags_count) {
+int unsubscribe(int client_socket, char *tag_name, char ***tags, int *tags_count, int ***subs, int **subs_count) {
   int i;
   int tag_index = -1;
-  for (i = 0; i <= *tags_count; i++) {
-    if (strcmp(tags[i].name, tag_name) == 0) {
+  for (i = 0; i < *tags_count; i++) {
+    if (strcmp(tag_name, (*tags)[i]) == 0) {
       tag_index = i;
     }
   }
@@ -62,22 +64,23 @@ int unsubscribe(int client_socket, char *tag_name, struct Tag *tags, int *tags_c
     return 0;
   } else {
     int client_index = -1;
-    for (i = 0; i <= tags[tag_index].subscriptions_count; i++) {
-      if (tags[tag_index].subscriptions[i] == client_socket) {
+    for (i = 0; i < (*subs_count)[tag_index]; i++) {
+      if ((*subs)[tag_index][i] == client_socket) {
         client_index = i;
       }
     }
     if (client_index != -1) {
-      int *aux = malloc(tags[tag_index].subscriptions_count - 1 * sizeof(int));
+      int *aux = malloc((*subs_count)[tag_index] * sizeof(int));
       int j = 0;
-      for (i = 0; i <= tags[tag_index].subscriptions_count; i++) {
-        if (tags[tag_index].subscriptions[i] != client_socket) {
-          aux[j] = tags[tag_index].subscriptions[i];
+      for (i = 0; i < (*subs_count)[tag_index]; i++) {
+        if ((*subs)[tag_index][i] != client_socket) {
+          aux[j] = (*subs)[tag_index][i];
           j++;
         }
       }
-      tags[tag_index].subscriptions = aux;
-      tags[tag_index].subscriptions_count = tags[tag_index].subscriptions_count - 1;
+      memcpy(((*subs)[tag_index]), aux, sizeof(aux));
+      (*subs_count)[tag_index] = (*subs_count)[tag_index] - 1;
+      free(aux);
       return 1;
     } else {
       return 0;
@@ -87,13 +90,30 @@ int unsubscribe(int client_socket, char *tag_name, struct Tag *tags, int *tags_c
 
 int main(int argc, char **argv) {
   int tags_count = 0;
-  struct Tag *tags = malloc(tags_count * sizeof(Tag));
-  subscribe(1, "dota", &tags, 0);
-  subscribe(1, "overwatch", &tags, 1);
-  subscribe(2, "dota", &tags, 2);
-  subscribe(3, "dota", &tags, 2);
-  struct Tag tag2 = tags[1];
-  printf("\n%s, %ls\n", tag2.name, tag2.subscriptions);
-  struct Tag tag1 = tags[0];
-  printf("\n%s, %d\n", tag1.name, tag1.subscriptions_count);
+  char **tags = malloc(tags_count * sizeof(char *));
+  int **subs = malloc(tags_count * sizeof(int *));
+  int *subs_count = malloc(tags_count * sizeof(int));
+
+  subscribe(1, "dota", &tags, &tags_count, &subs, &subs_count);
+  subscribe(5, "overwatch", &tags, &tags_count, &subs, &subs_count);
+  subscribe(5, "overwatch", &tags, &tags_count, &subs, &subs_count);
+  subscribe(50, "overwatch", &tags, &tags_count, &subs, &subs_count);
+  subscribe(5, "dota", &tags, &tags_count, &subs, &subs_count);
+  subscribe(3, "dota", &tags, &tags_count, &subs, &subs_count);
+  unsubscribe(3, "dota", &tags, &tags_count, &subs, &subs_count);
+  unsubscribe(3, "dota", &tags, &tags_count, &subs, &subs_count);
+  subscribe(3, "dota", &tags, &tags_count, &subs, &subs_count);
+
+  int j;
+  for (j = 0; j < tags_count; j++) {
+    printf("\n\tTag: %s\nClients Subscribed: ", tags[j]);
+    int i;
+    for (i = 0; i < subs_count[j]; i++) {
+      printf("%d", subs[j][i]);
+      if (i != subs_count[j] - 1) {
+        printf(", ");
+      }
+    }
+    printf("\n");
+  }
 }
